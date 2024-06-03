d1f2589a619d733fbbe3c54220d2f3f9fabdfc73
MoneyAsk <singhmanishit@gmail.com>
2024-06-03 22:30:46 +0530

    1 commit

---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
+export const inter = Inter({subsets: ['latin'],weight: "500"});
+export const dm_sans = DM_Sans({subsets: ['latin'],weight: "500"});
